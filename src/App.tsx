0eb10e6292aeb20152be0dcb9000f1b32137d7bb
Márcio Mingante <marciomvp17@gmail.com>
2024-05-28 21:36:03 -0300

    conclui req 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import { getAnimeListAPI } from './services/api';
+import FilmsContext from './context/FilmsContext';
 
 function App() {
+  const [animeList, setAnimeList] = useState();
+
+  useEffect(() => {
+    const handleAPIInfo = async () => {
+      const data = await getAnimeListAPI();
+
+      setAnimeList(data);
+    };
+
+    handleAPIInfo();
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" Component={ Home } />
-      <Route path="/favorites" Component={ Favorites } />
-    </Routes>
+    <FilmsContext.Provider value={ { animeList } }>
+      <Routes>
+        <Route path="/" Component={ Home } />
+        <Route path="/favorites" Component={ Favorites } />
+      </Routes>
+    </FilmsContext.Provider>
   );
 }
 
--- src/context/FilmsContext.tsx
+++ src/context/FilmsContext.tsx
@@ -1,1 +1,5 @@
-// implemente a estrutura do context aqui.
+import { createContext } from 'react';
+
+const FilmsContext = createContext();
+
+export default FilmsContext;
